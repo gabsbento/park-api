83789ec4162345e227e622511795c662b23e1722
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-13 15:20:54 -0300

    :sparkles: feat: repository, service e controller de usuario

---
+++ src/main/java/com/gabriel/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.gabriel.demo_park_api.service;
+
+import com.gabriel.demo_park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+}

1eea7e4940971c7dfe6b2c447f45b65a950e28e9
Gabriel Bento <gab.silva.bento@gmail.com>
2025-01-27 14:51:59 -0300

    :sparkles: feat: padrao dto na criacao de usuario

--- src/main/java/com/gabriel/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/gabriel/demo_park_api/web/controller/UsuarioController.java
@@ -2,6 +2,9 @@ package com.gabriel.demo_park_api.web.controller;
 
 import com.gabriel.demo_park_api.entity.Usuario;
 import com.gabriel.demo_park_api.service.UsuarioService;
+import com.gabriel.demo_park_api.web.dto.UsuarioCreateDto;
+import com.gabriel.demo_park_api.web.dto.UsuarioResponseDto;
+import com.gabriel.demo_park_api.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +19,9 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
-        Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto usuarioCreateDto){
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(usuarioCreateDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")

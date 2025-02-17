26707cd404820e63cc98213e2561862fda2caa2a
Gabriel Bento <gab.silva.bento@gmail.com>
2025-02-17 15:32:30 -0300

    :sparkles: feat: configurando ambiente de teste

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into usuarios(id, username, password, role) values(100, "gabriel@gmail.com", "123456", "ROLE_ADMIN");
+insert into usuarios(id, username, password, role) values(101, "daniel@gmail.com", "123456", "ROLE_CLIENT");
+insert into usuarios(id, username, password, role) values(102, "ana@gmail.com", "123456", "ROLE_CLIENT");

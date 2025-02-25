5fecf828766732b1b5b5a46169db91179546e577
Gabriel Bento <gab.silva.bento@gmail.com>
2025-02-25 15:16:33 -0300

    :sparkles: feat: classe jwt user details service

---
+++ src/main/java/com/gabriel/demo_park_api/jwt/JwtUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.gabriel.demo_park_api.jwt;
+
+import com.gabriel.demo_park_api.entity.Usuario;
+import com.gabriel.demo_park_api.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    private UsuarioService usuarioService;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.buscarPorUsername(username);
+        return new JwtUserDetails(usuario);
+    }
+
+    public JwtToken getTokenAuthenticated(String username){
+        Usuario.Role role = usuarioService.buscarRolePorUsername(username);
+        return JwtUtils.createToken(username, role.name().substring("ROLE_".length()));
+    }
+}
--- src/main/java/com/gabriel/demo_park_api/repository/UsuarioRepository.java
+++ src/main/java/com/gabriel/demo_park_api/repository/UsuarioRepository.java
@@ -2,6 +2,13 @@ package com.gabriel.demo_park_api.repository;
 
 import com.gabriel.demo_park_api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+
+    @Query("select u.role from usuario u where u.username like :username")
+    Usuario.Role findRoleByUsername(String username);
 }

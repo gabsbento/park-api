9451b978ec231a4f83145188d86f19ba5a50a232
Gabriel Bento <gab.silva.bento@gmail.com>
2025-02-25 15:03:47 -0300

    :card_file_box: raw: retirada de imports não utilizados

--- src/main/java/com/gabriel/demo_park_api/jwt/JwtUserDetails.java
+++ src/main/java/com/gabriel/demo_park_api/jwt/JwtUserDetails.java
@@ -1,11 +1,8 @@
 package com.gabriel.demo_park_api.jwt;
 
 import com.gabriel.demo_park_api.entity.Usuario;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
-
-import java.util.Collection;
 
 public class JwtUserDetails extends User {
     private Usuario usuario;
